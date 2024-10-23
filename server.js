a3930476c8c058efa62df5e7369c106d521b751b
jackxsudipto <98466832+jackxsudipto@users.noreply.github.com>
2024-10-23 22:02:44 +0530

    .

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import express from 'express';
 import path from 'path';
 import multer from 'multer';
 import { mergePdfs } from './merge.js';
+import { fileURLToPath } from 'url';
+
 
 const app = express()
 const upload = multer({dest:'uploads/'})
 const port = 3000
+let mergedFile 
 
 app.use('/static', express.static('public')) 
 app.get('/', (req, res) => {
@@ -14,10 +17,15 @@ app.get('/', (req, res) => {
 
 app.post('/merge', upload.array('pdfs',2), async (req, res, next) => {
     console.log(req.files)
-    let mergedFile = await mergePdfs(`uploads/${req.files[0].filename}`, `uploads/${req.files[1].filename}`)
-    res.redirect(`http://localhost:3000/static/${mergedFile}.pdf`)
+    mergedFile = await mergePdfs(`uploads/${req.files[0].filename}`, `uploads/${req.files[1].filename}`)
+    // res.sendFile(path.resolve('templates/index.html'))
+    // res.redirect(`http://localhost:3000/static/${mergedFile}.pdf`)
 })
 
 app.listen(port, ()=>{
     console.log(`App is running at http://localhost:${port}`)
+})
+
+app.get('/preview',(req, res)=>{
+  res.redirect(`http://localhost:${port}/static/${mergedFile}.pdf`)
 })
